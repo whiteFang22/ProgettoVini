2d97363bbcef58503f1b982216c528b2700903e7
fedecardelli <fedecardelli@gmail.com>
2023-09-28 17:11:51 +0200

    commit2

--- src/main/java/com/example/client/FiltroController.java
+++ src/main/java/com/example/client/FiltroController.java
@@ -1,7 +1,7 @@
 package com.example.client;
 
 import javafx.fxml.FXML;
-
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
