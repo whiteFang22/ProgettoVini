53ab34f169ccbd6349b9467b6af8db040e8869a0
fedecardelli <123898838+fedecardelli@users.noreply.github.com>
2023-10-16 18:31:05 +0200

    Risoluzione errori (#9)

--- src/main/java/com/example/classes/FiltriRicerca.java
+++ src/main/java/com/example/classes/FiltriRicerca.java
@@ -3,5 +3,5 @@ package com.example.classes;
 import java.io.Serializable;
 import java.util.Date;
 
-public record FiltriRicerca(Date data1, Date data2, String nome, int annoProduzione) implements Serializable {
+public record FiltriRicerca(Date data1, Date data2, String nome, Integer annoProduzione) implements Serializable {
 }
